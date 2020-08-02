f6c040adff0a8f9077fabe3e2e31317d0598ef10
Randy Oest <oest@amazingrando.com>
2020-08-01 21:07:53 -0400

    youtube fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,15 +29,30 @@ const IndexPage = () => (
       won’t forget. If you and your friends have a good time, you might even
       continue the story for years to come.
     </p>
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube-nocookie.com/embed/_vYcuIWOWNc"
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-      title="video"
-    ></iframe>
+    <div
+      className="video"
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0,
+      }}
+    >
+      <iframe
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        src={`https://www.youtube.com/embed/_vYcuIWOWNc`}
+        title="video"
+      />
+    </div>
   </Layout>
 )
 
